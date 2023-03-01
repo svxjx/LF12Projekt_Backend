0df502614f2b94b8c8cfbac4c6c01825e42914a1
duplooo <username@users.noreply.github.com>
2023-03-01 11:50:19 +0100

    new api path for topics

--- index.js
+++ index.js
@@ -43,6 +43,14 @@ app.post("/api/:session/answer", (req, res) => {
 	res.end()
 })
 
+app.get("/api/topics", (req, res) => {
+	con.query("SELECT * FROM topic ORDER BY RAND() LIMIT 10", (err, results) => {
+		if (err) return console.error(err)
+		if (!results[0] || results[0].length < 1) return console.log("No results")
+		res.send(results)
+	})
+})
+
 /**
  *
  * @param {Number} groupId
